148d4239272fee957b4c8aa5dfac44cdf2c59ccb
Ben Gelb <beg5670@gmail.com>
2012-11-09 11:24:53 -0500

    changed filename

--- goro_hungry.py
+++ goro_hungry.py
@@ -7,7 +7,7 @@ from numpy import mean,std,array
 from collections import defaultdict
 from time import sleep
 
-filename = '/Users/adkatrit/Desktop/nasdaqlisted.txt'
+filename = '/Users/pwzoii/Documents/git_hub/goro/Goro-Invest/nasdaqlisted.txt'
 url = 'http://ichart.finance.yahoo.com/table.csv?a=00&b=1&c=2011&d=11&e=31&f=2011&g=d&ignore=.csv&s='
 url_YTD = 'http://ichart.finance.yahoo.com/table.csv?a=10&b=8&c=2011&d=10&e=8&f=2012&g=d&ignore=.csv&s='
 
