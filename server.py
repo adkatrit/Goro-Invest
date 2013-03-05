435e91ae97dfa63ee50b20d4e952dc78eccc1a9c
Ben Gelb <beg5670@gmail.com>
2013-03-04 23:26:09 -0500

    pass stock info to index page

--- server.py
+++ server.py
@@ -5,20 +5,26 @@ from goro import FinanceStream, YahooStream
 import settings
 
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        pass
-
-
-def setup():
-    fnames = [settings._sharpe, settings._range]
+def render_stock_data():
+    stock_attr = {}
     fs = FinanceStream()
+    fnames = [settings._sharpe, settings._range]
     stock_data, range_data = [fs.load_data(fn) for fn in fnames]
 
     for sd, rd in zip(stock_data, range_data):
-        ys = YahooStream(sd[0], sd[1])
-        for x in ys.open_conn():
-            print x
+            ys = YahooStream(sd[0], sd[1])
+            for x in ys.open_conn():
+                stock_attr[ys.symbol] = x
+
+    return stock_attr
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        # Dont run -- throwing HTTP error
+        stock_attr = render_stock_data()
+        self.write(stock_attr)
+
 
 application = tornado.web.Application([
     (r"/", MainHandler),
